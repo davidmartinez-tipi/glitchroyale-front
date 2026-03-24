43c8a433587fbcacf7a551a663d5bbb860facde5
David Martinez <david.martinez@tipi.com.co>
2026-03-24 15:34:02 -0500

    Fix: force vite build without tsc

--- glitchroyale-front/vite.config.ts
+++ glitchroyale-front/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 1. Asegura que las rutas empiecen desde la raíz
+  base: '/', 
+  build: {
+    // 2. Confirmamos que la carpeta de salida es 'dist'
+    outDir: 'dist',
+    // 3. Esto ayuda a limpiar archivos viejos antes de cada build
+    emptyOutDir: true,
+  }
 })
